161ee0823923748b36ccabc082b4092b65142a77
Aman Mathur <amanmathur@SnackNations-MacBook-Pro-53.local>
2019-03-05 11:01:15 -0800

    - adding code for visualization and selecting K

--- Diversified Portfolio Clustering/extracting_stocks_data.R
+++ Diversified Portfolio Clustering/extracting_stocks_data.R
@@ -1,6 +1,9 @@
 library(Quandl)
 library(tidyverse)
 library(readr)
+library(factoextra)
+library(cluster)
+library(gridExtra)
 
 set.seed(20)
 
@@ -16,24 +19,33 @@ DELAY_MODELS = 8
 # fundamentals_data <- Quandl.datatable("SHARADAR/SF1")
 # write_csv(fundamentals_data,'companies_fundamentals_data.csv')
 
-fundamentals<- read_csv("companies_fundamentals_data.csv")
+fundamentals <- read_csv("companies_fundamentals_data.csv")
 
 #select tickrs that have 8 years worth of data
-qualifying_tickers <- names(which(table(fundamentals$ticker) == DELAY_MODELS))
+qualifying_tickers <-
+  names(which(table(fundamentals$ticker) == DELAY_MODELS))
 
 #subset the data
-fundamentals_qualified <- subset(fundamentals, fundamentals$ticker %in% qualifying_tickers)
+fundamentals_qualified <-
+  subset(fundamentals, fundamentals$ticker %in% qualifying_tickers)
 
 
 #aggregate data by avg over income, revenue and assets over 10 years by ticker name
-aggregate_by_ticker <- aggregate(fundamentals_qualified[c( "assets", "netinccmnusd", "revenueusd")], 
-                                 by = list(fundamentals_qualified$ticker), 
-                                 mean)
+aggregate_by_ticker <-
+  aggregate(fundamentals_qualified[c("assets", "netinccmnusd", "revenueusd")],
+            by = list(fundamentals_qualified$ticker),
+            mean)
 
 
-aggregate_by_ticker$avg_income_ratio <- round(aggregate_by_ticker$netinccmnusd/aggregate_by_ticker$assets,2)
-aggregate_by_ticker$avg_revenue_ratio <- round(aggregate_by_ticker$revenueusd/aggregate_by_ticker$assets,2)
-aggregate_by_ticker$weighted_avg <- (aggregate_by_ticker$avg_revenue_ratio * WEIGHT) + (aggregate_by_ticker$avg_income_ratio * (1-WEIGHT))
+aggregate_by_ticker$avg_income_ratio <-
+  round(aggregate_by_ticker$netinccmnusd / aggregate_by_ticker$assets,
+        2)
+aggregate_by_ticker$avg_revenue_ratio <-
+  round(aggregate_by_ticker$revenueusd / aggregate_by_ticker$assets,
+        2)
+aggregate_by_ticker$weighted_avg <-
+  (aggregate_by_ticker$avg_revenue_ratio * WEIGHT) + (aggregate_by_ticker$avg_income_ratio * (1 -
+                                                                                                WEIGHT))
 
 
 head(aggregate_by_ticker)
@@ -41,21 +53,73 @@ head(aggregate_by_ticker)
 colSums(is.na((aggregate_by_ticker)))
 
 # Drop All NAs
-aggregate_by_ticker<- drop_na(aggregate_by_ticker)
+aggregate_by_ticker <- drop_na(aggregate_by_ticker)
 
 #Double Check
 colSums(is.na((aggregate_by_ticker)))
 head(aggregate_by_ticker)
 
 
-clusters <- kmeans(aggregate_by_ticker[,7],5)
-aggregate_by_ticker$cluster <- clusters$cluster
+# creating clusters with centers ranging from 2 to 7
+
+clusters_2 <- kmeans(aggregate_by_ticker[, 7], 2, nstart = 25)
+clusters_3 <- kmeans(aggregate_by_ticker[, 7], 3, nstart = 25)
+clusters_4 <- kmeans(aggregate_by_ticker[, 7], 4, nstart = 25)
+clusters_5 <- kmeans(aggregate_by_ticker[, 7], 5, nstart = 25)
+clusters_6 <- kmeans(aggregate_by_ticker[, 7], 6, nstart = 25)
+clusters_7 <- kmeans(aggregate_by_ticker[, 7], 7, nstart = 25)
+
+# creating visualizations
+
+p2 <- fviz_cluster(clusters_2, aggregate_by_ticker[, 2:7])
+p3 <- fviz_cluster(clusters_3, aggregate_by_ticker[, 2:7])
+p4 <- fviz_cluster(clusters_4, aggregate_by_ticker[, 2:7])
+p5 <- fviz_cluster(clusters_5, aggregate_by_ticker[, 2:7])
+p6 <- fviz_cluster(clusters_6, aggregate_by_ticker[, 2:7])
+p7 <- fviz_cluster(clusters_7, aggregate_by_ticker[, 2:7])
 
 
 
-# #create a new dataframe with ticker information and AVG revenue, income and assests value
-# 
-# analytical_dataset <- as.data.frame(qualifying_tickers)
-# 
-# #append avg assests data across 8 years
-# 
+
+# visualize different clusters
+grid.arrange(p5, p2, p3, p4, p6, p7, nrow = 3)
+
+
+
+
+# determine the right number of K
+
+# Using WSS
+fviz_nbclust(aggregate_by_ticker[, 2:7], kmeans, method = "wss")
+
+# Using silhouette
+fviz_nbclust(aggregate_by_ticker[, 2:7], kmeans, method = "silhouette")
+
+# using Gap Stat
+gap_stat <-
+  clusGap(
+    aggregate_by_ticker[, 2:7],
+    FUN = kmeans,
+    nstart = 25,
+    K.max = 10,
+    B = 50
+  )
+fviz_gap_stat(gap_stat)
+
+
+
+
+
+######## Unused code
+# table(clusters$cluster)
+# aggregate_by_ticker$cluster <- clusters$cluster
+#
+#
+# ggplot(
+#   aggregate_by_ticker,
+#   aes(
+#     aggregate_by_ticker$avg_income_ratio,
+#     aggregate_by_ticker$avg_revenue_ratio,
+#     color = as.factor(aggregate_by_ticker$cluster)
+#   )
+# ) + geom_point()
