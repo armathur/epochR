4e88b30b108f3883b014ba14c1b5da0882701a6f
Aman Mathur <armathur@usc.edu>
2019-03-06 00:17:07 -0800

    - added stash tickers

--- Diversified Portfolio Clustering/extracting_stocks_data.R
+++ Diversified Portfolio Clustering/extracting_stocks_data.R
@@ -5,19 +5,17 @@ library(factoextra)
 library(cluster)
 library(gridExtra)
 
-<<<<<<< HEAD
 #Set Quandl API
-Quandl.api_key(Sys.getenv("API_QUANDL"))
+# Quandl.api_key(Sys.getenv("API_QUANDL"))
 
 
 #fetch data
-fundamentals_data <- Quandl.datatable("SHARADAR/SF1", paginate = T)
-
-
-write_csv(fundamentals_data,'companies_fundamentals_data.csv')
-=======
-set.seed(20)
->>>>>>> 937317abee09c20b611ef5704932c9654e59e271
+# fundamentals_data <- Quandl.datatable("SHARADAR/SF1", paginate = T)
+# 
+# 
+# write_csv(fundamentals_data,'companies_fundamentals_data.csv')
+# 
+# set.seed(20)
 
 # Quandl.api_key(Sys.getenv("API_QUANDL"))
 # Constatnt value assigned to revenue
@@ -32,6 +30,8 @@ DELAY_MODELS = 8
 # write_csv(fundamentals_data,'companies_fundamentals_data.csv')
 
 fundamentals <- read_csv("companies_fundamentals_data.csv")
+
+
 
 #select tickrs that have 8 years worth of data
 qualifying_tickers <-
@@ -117,6 +117,100 @@ gap_stat <-
     B = 50
   )
 fviz_gap_stat(gap_stat)
+
+
+
+#### FOR STASH ::::::
+
+# Subset only tickers that are in Stash
+fundamentals_stash <-
+  subset(fundamentals, fundamentals$ticker %in% stash_tickers)
+
+# find qualifying tickers (with 8 years worth of data)
+
+qualifying_tickers_stash <-
+  names(which(table(fundamentals_stash$ticker) == DELAY_MODELS))
+
+
+fundamentals_stash <-
+  subset(fundamentals_stash, fundamentals_stash$ticker %in% qualifying_tickers_stash)
+
+
+
+aggregate_by_ticker_stash <-
+  aggregate(fundamentals_stash[c("assets", "netinccmnusd", "revenueusd")],
+            by = list(fundamentals_stash$ticker),
+            mean)
+
+
+aggregate_by_ticker_stash$avg_income_ratio <-
+  round(aggregate_by_ticker_stash$netinccmnusd / aggregate_by_ticker_stash$assets,
+        2)
+aggregate_by_ticker_stash$avg_revenue_ratio <-
+  round(aggregate_by_ticker_stash$revenueusd / aggregate_by_ticker_stash$assets,
+        2)
+aggregate_by_ticker_stash$weighted_avg <-
+  (aggregate_by_ticker_stash$avg_revenue_ratio * WEIGHT) + (aggregate_by_ticker_stash$avg_income_ratio * (1 -
+                                                                                                WEIGHT))
+
+
+head(aggregate_by_ticker_stash)
+#check num of NAs
+colSums(is.na((aggregate_by_ticker_stash)))
+
+# Drop All NAs
+aggregate_by_ticker_stash <- drop_na(aggregate_by_ticker_stash)
+
+#Double Check
+colSums(is.na((aggregate_by_ticker_stash)))
+head(aggregate_by_ticker_stash)
+
+
+# Finally we have about 118 tickers to work with
+
+
+# determine the right number of K
+
+# Using WSS
+fviz_nbclust(aggregate_by_ticker_stash[, 2:7], kmeans, method = "wss")
+
+# Using silhouette
+fviz_nbclust(aggregate_by_ticker_stash[, 2:7], kmeans, method = "silhouette")
+
+# using Gap Stat
+gap_stat <-
+  clusGap(
+    aggregate_by_ticker_stash[, 2:7],
+    FUN = kmeans,
+    nstart = 25,
+    K.max = 10,
+    B = 50
+  )
+fviz_gap_stat(gap_stat)
+
+
+clusters_2 <- kmeans(aggregate_by_ticker_stash[, 7], 2, nstart = 25)
+clusters_3 <- kmeans(aggregate_by_ticker_stash[, 7], 3, nstart = 25)
+clusters_4 <- kmeans(aggregate_by_ticker_stash[, 7], 4, nstart = 25)
+clusters_5 <- kmeans(aggregate_by_ticker_stash[, 7], 5, nstart = 25)
+clusters_6 <- kmeans(aggregate_by_ticker_stash[, 7], 6, nstart = 25)
+clusters_7 <- kmeans(aggregate_by_ticker_stash[, 7], 7, nstart = 25)
+
+# creating visualizations
+
+p2 <- fviz_cluster(clusters_2, aggregate_by_ticker_stash[, 2:7])
+p3 <- fviz_cluster(clusters_3, aggregate_by_ticker_stash[, 2:7])
+p4 <- fviz_cluster(clusters_4, aggregate_by_ticker_stash[, 2:7])
+p5 <- fviz_cluster(clusters_5, aggregate_by_ticker_stash[, 2:7])
+p6 <- fviz_cluster(clusters_6, aggregate_by_ticker_stash[, 2:7])
+p7 <- fviz_cluster(clusters_7, aggregate_by_ticker_stash[, 2:7])
+
+
+
+
+# visualize different clusters
+grid.arrange(p2, p3, p4, p5, p6, p7, nrow = 3)
+
 
 
 
