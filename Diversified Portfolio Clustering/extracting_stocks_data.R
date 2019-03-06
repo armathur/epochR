7fc9cb1afa42c3615631c89c370fba602cde7446
Aman Mathur <armathur@usc.edu>
2019-03-05 20:36:28 -0800

    - m

--- Diversified Portfolio Clustering/extracting_stocks_data.R
+++ Diversified Portfolio Clustering/extracting_stocks_data.R
@@ -2,7 +2,13 @@ library(Quandl)
 library(tidyverse)
 library(readr)
 
+#Set Quandl API
 Quandl.api_key(Sys.getenv("API_QUANDL"))
-fundamentals_data <- Quandl.datatable("SHARADAR/SF1")
+
+
+#fetch data
+fundamentals_data <- Quandl.datatable("SHARADAR/SF1", paginate = T)
+
+
 write_csv(fundamentals_data,'companies_fundamentals_data.csv')
 
